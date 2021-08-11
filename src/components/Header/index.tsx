7e2c96f5e7288e3d7fc2331b89b54af72af836c9
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-08-11 08:56:40 -0300

    feat: :sparkles: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+import Logo from '../../../public/images/logo.svg';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={Logo} alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
