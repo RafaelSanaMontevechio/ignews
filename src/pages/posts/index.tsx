4b2262c10f70941f6e1d37b707b8fa75033b21fe
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-09-29 08:39:05 -0300

    feat: :poop: get post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
 
+import Link from 'next/link';
+
 import Head from 'next/head';
 
 import Prismic from '@prismicio/client';
@@ -31,11 +33,13 @@ export default function Posts({ posts }: PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
