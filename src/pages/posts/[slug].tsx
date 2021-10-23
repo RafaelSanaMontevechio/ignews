da0343d97e2b246d5d4f51f5fabe998686dd6b12
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-10-23 10:46:23 -0300

    feat: :sparkles: add post previw page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -48,7 +48,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   console.log(session);
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
